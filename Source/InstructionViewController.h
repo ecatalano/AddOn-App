ce1f0d97437d1136f1ef9299a26c37ab1c0d6c03
ecatalano <ecatala1@binghamton.edu>
2015-12-04 10:57:40 -0500

    added instruction images to resources + saving before attempting to use pageviewcontroller for instructions screen

---
+++ Source/InstructionViewController.h
@@ -0,0 +1,13 @@
+//
+//  InstructionViewController.h
+//  AddOn
+//
+//  Created by Elliot Catalano on 12/4/15.
+//  Copyright © 2015 Elliot Catalano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InstructionViewController : UIPageViewController
+
+@end
