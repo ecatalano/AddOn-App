6946964ee645ed1e9f8544ae02c91a01f3253b10
ecatalano <ecatala1@binghamton.edu>
2015-12-11 11:35:51 -0500

    added app icons and splash screen to image classes

--- Source/Tile.m
+++ Source/Tile.m
@@ -39,7 +39,7 @@
 
 -(void) selectTile{
     self.isSelected = true;
-    NSLog(@"Selected Tile: %d, %d",self.x, self.y);
+    //NSLog(@"Selected Tile: %d, %d",self.x, self.y);
     _backgroundNode.color = [CCColor magentaColor];
     _backgroundNode.opacity = .3;
 }
